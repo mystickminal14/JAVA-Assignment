6c5c6defeea7fae87d542e76bc285f30d5cd0986
MINAL <121595966+mystickminal14@users.noreply.github.com>
2023-01-29 00:39:50 +0545

    Delete javac Largename3.java

--- javac Largename3.java
+++
@@ -1,8 +0,0 @@
-public class Largename3{
-public static void main(String [] args){
-System.out.println(" ^-^     -----"); 
-System.out.println("(' ')  / Hello |\'");
-System.out.println("( - ) <  junior |");
-System.out.println(" |||   \  coder!/");
-System.out.println("(_|_|)    -----");
-}}
